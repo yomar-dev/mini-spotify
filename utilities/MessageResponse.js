ff0c2e1ce134423bc43ec28dae8c87bc679cfe0b
Yomar Miranda <mirandayomar@gmail.com>
2018-07-12 17:28:56 -0500

    Mensajes de respuesta de la App.

---
+++ utilities/MessageResponse.js
@@ -0,0 +1,5 @@
+'use_strict'
+module.exports = {
+  'MESSAGE_ERROR_FIELDS': { status: false, message: 'Todos los campos son obligatorios.' },
+  'MESSAGE_PERMIT_ROUTE': { status: false, message: 'No tienes permiso para realizar estar operación.' }
+};
