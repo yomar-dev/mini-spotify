e04b0053012c318b1a83092d1c507f94eaf5dd34
Yomar Miranda <mirandayomar@gmail.com>
2018-07-12 17:36:25 -0500

    Controlador User - Save User.

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,26 @@
 'use strict'
 
+const bcrypt = require('bcrypt-nodejs');
+const User = require('../models/user');
 
+function saveUser(req, res){
+    const params = req.body;
+    bcrypt.hash(params.password, null, null, ((err, hash) => {
+        if (err) {
+            return res.status(500).send({ message: 'Error en la peticion.' });
+        } else {
+            params.password = hash;
+            User.create(params, (error, response)=> {
+                if(error){
+                    res.status(200).send({ message: "Problemas al guardar el registro." });
+                }else{
+                    res.status(200).send({ message: "Registro guardado con éxito." });
+                }
+            })
+        }
+    }));
+}
 
 module.exports = {
-    
+    saveUser
 }
