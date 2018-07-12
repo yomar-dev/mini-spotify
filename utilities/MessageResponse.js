201988c203fa30a632b9ee5749a313ff124b3dd6
Yomar Miranda <mirandayomar@gmail.com>
2018-07-12 18:02:58 -0500

    Nuevo mensaje de error para campos invalidos.

--- utilities/MessageResponse.js
+++ utilities/MessageResponse.js
@@ -1,5 +1,6 @@
 'use_strict'
 module.exports = {
   'MESSAGE_ERROR_FIELDS': { status: false, message: 'Todos los campos son obligatorios.' },
+  'MESSAGE_INVALID_FIELD': { status: false, message: 'Campos invalidos.' },
   'MESSAGE_PERMIT_ROUTE': { status: false, message: 'No tienes permiso para realizar estar operación.' }
 };
