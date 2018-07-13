4aa7019a795f9aca42ddc3dc72ba8ebbc74be46f
Yomar Miranda <mirandayomar@gmail.com>
2018-07-12 20:17:08 -0500

    Evitar que se muestren los mensajes de errores por consola.

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -17,7 +17,6 @@ exports.ensureAuth = function(req, res, next){
             return res.status(401).send({ message: "El token ha expirado." });
         }
     }catch(ex){
-        console.log(ex);
         return res.status(404).send({ message: "Token no valido." });
     }
     req.user = payload;
