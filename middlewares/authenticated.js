5c59e057d2997e0d924f93ebba932ecf1295ea29
Yomar Miranda <mirandayomar@gmail.com>
2018-07-12 19:15:54 -0500

    Middleware para la autenticación de los usuarios.

---
+++ middlewares/authenticated.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const jwt = require('jwt-simple');
+const moment = require('moment');
+const secret = 'mini_spotify';
+
+exports.ensureAuth = function(req, res, next){
+    let payload = '';
+    if(!req.headers.authorization){
+        return res.status(403).send({ message: "La peticion no contine la cabecera de autenticación." });
+    }
+
+    const token = req.headers.authorization.replace(/['"]+/g, '');
+    try{
+        payload = jwt.decode(token, secret);
+        if(payload.exp <= moment().unix()){
+            return res.status(401).send({ message: "El token ha expirado." });
+        }
+    }catch(ex){
+        console.log(ex);
+        return res.status(404).send({ message: "Token no valido." });
+    }
+    req.user = payload;
+    next();
+}
