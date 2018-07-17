af515282acb039ee996ca17933069a73c5d7bb15
Yomar Miranda <mirandayomar@gmail.com>
2018-07-17 01:14:44 -0500

    Router User - Get Image.

--- routes/user.js
+++ routes/user.js
@@ -34,6 +34,7 @@ api.post('/user/login', celebrate({
 
 api.put('/user/update/:id', md_auth.ensureAuth, UserController.updateUser);
 api.post('/user/upload-image/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
+api.get('/user/image/:imageFile', UserController.getImage);
 
 api.get('/user/test', md_auth.ensureAuth, UserController.test);
 
