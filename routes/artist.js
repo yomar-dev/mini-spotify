41503a2eee5d602ebae923a2dc0c7d39f14eabe2
Yomar Miranda <mirandayomar@gmail.com>
2018-07-17 02:19:18 -0500

    Router Artist - Save Artist.

--- routes/artist.js
+++ routes/artist.js
@@ -8,6 +8,15 @@ const ArtistController = require('../controllers/artist');
 
 const api = express.Router();
 
+/*api.post('/artist/save', celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required(),
+        description: Joi.string().required()
+    }).unknown()
+}), (error, req, res, next) => {
+    res.status(200).send(MessageError.MESSAGE_INVALID_FIELD);
+}, ArtistController.saveArtist);*/
 
+api.post('/artist/save', ArtistController.saveArtist);
 
 module.exports = api;
