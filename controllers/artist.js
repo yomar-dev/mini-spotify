dcbc4efd81dbb7e70073c5b05abaf4003996c58a
Yomar Miranda <mirandayomar@gmail.com>
2018-07-17 02:18:23 -0500

    Controlador Artist - Save Artist.

--- controllers/artist.js
+++ controllers/artist.js
@@ -2,3 +2,23 @@
 
 const fs = require('fs');
 const path = require('path');
+
+const Artist = require('../models/artist');
+const Album = require('../models/album');
+const Song = require('../models/song');
+
+function saveArtist(req, res){
+    const params = req.body;
+
+    Artist.create(params, (err, response) => {
+        if(err){
+            res.status(200).send({ message: "Problemas al guardar el artista." });
+        }else{
+            res.status(200).send({ message: "Artista registrado con éxito.", data: response });
+        }
+    });
+}
+
+module.exports = {
+    saveArtist
+}
