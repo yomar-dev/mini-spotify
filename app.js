c07c139c213de6a604d5c405d2d00d8b75de150d
Yomar Miranda <mirandayomar@gmail.com>
2018-07-23 23:50:37 -0500

    Cargando rutas - Album.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 // Cargar Rutas
 const user_routes = require('./routes/user');
 const artist_routes = require('./routes/artist');
+const album_routes = require('./routes/album');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,5 +19,6 @@ app.use(bodyParser.json());
 // Rutas Base
 app.use('/api', user_routes);
 app.use('/api', artist_routes);
+app.use('/api', album_routes);
 
 module.exports = app;
