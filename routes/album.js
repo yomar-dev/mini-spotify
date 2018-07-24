5937ead4d31eaa49be2bf113540e5577d1ec6998
Yomar Miranda <mirandayomar@gmail.com>
2018-07-23 23:50:15 -0500

    Rutas - Album.

---
+++ routes/album.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express');
+const { celebrate, Joi } = require('celebrate');
+const MessageError = require('../utilities/MessageResponse');
+const md_auth = require('../middlewares/authenticated');
+const AlbumController = require('../controllers/album');
+
+const multipart = require('connect-multiparty');
+const md_upload = multipart({ uploadDir: './uploads/album' });
+
+const api = express.Router();
+
+api.get('/album/', md_auth.ensureAuth, AlbumController.getAlbum);
+
+module.exports = api;
